ce7e77f4ae4566ee0cb0e51d7cdca5a33c692943
Julian Victoriano <jvictoriano35@yahoo.com>
2023-09-29 09:01:46 -0700

    remove duplicates

---
+++ Pointers/RemoveDuplicates.js
@@ -0,0 +1,11 @@
+let removeDuplicates = (nums) => {
+    let i = 0
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
